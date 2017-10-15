be3a86ee68320f7ba710baebfd7418099bf028ac
Rikesh Thapa <rikeshthapa@Rikeshs-MacBook-Pro.local>
2017-10-14 21:27:39 -0400

    Adding basic vector operations

---
+++ LinearAlgebra/vector.py
@@ -0,0 +1,61 @@
+import operator
+
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+
+
+def plus(v1, v2):
+    #v1 is vector 1 and v2 is vector 2, the order does not matter
+    resultantVector = tuple(map(operator.add, v1.coordinates, v2.coordinates))
+    return resultantVector
+
+def minus(v1, v2):
+    #v1 is initial v2 is deduction, order does matter
+    differenceVector = tuple(map(operator.sub, v1.coordinates, v2.coordinates))
+    return differenceVector
+
+def scalar(s, v):
+    #s is multiplier, v is vector
+    scalarresult = []
+    for i in range(0, v.dimension):
+        scalarresult.append(s*v.coordinates[i])
+        i+=1
+
+    return tuple(scalarresult)
+
+
+if __name__ == '__main__':
+    coordinates1 = (7.119, 8.215)
+    v1 = Vector(coordinates1)
+
+    coordinates3 = (-8.223, 0.878)
+    v3 = Vector(coordinates3)
+
+    print(minus(v1, v3))
+
+    '''coordinates2 = (1.671, -1.012, -0.318)
+    v2 = Vector(coordinates2)
+
+    print(scalar(7.41, v2))'''
+
+
