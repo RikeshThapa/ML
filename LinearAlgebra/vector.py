3064902a02e56846264fe1b1bc32d11ca4f71a48
Rikesh Thapa <rikeshthapa@Rikeshs-MacBook-Pro.local>
2017-10-20 19:11:22 -0400

    adding dotproduct functionality, including angle calculation between two vectors

--- LinearAlgebra/vector.py
+++ LinearAlgebra/vector.py
@@ -49,10 +49,12 @@ class Vector(object):
 
     def getDirection(self):
         try:
-            #unit_vector = [(1/self.magnitude)*x for x in self.coordinates]
-            return self.times_scalar(1./self.magnitude)
+            unit_vector = [(1/self.magnitude)*x for x in self.coordinates]
+            return unit_vector
+            #return self.times_scalar(1./self.magnitude)
         except ZeroDivisionError:
             raise Exception("Cannot be normalized magnitude of vector is 0")
+
 
 
 def plus(v1, v2):
@@ -71,15 +73,37 @@ def scalar(s, v):
     for i in range(0, v.dimension):
         scalarresult.append(s*v.coordinates[i])
         i+=1
-
     return tuple(scalarresult)
+
+def getDotProduct(v1, v2):
+    # we do not know the angle between the vectors, so we will be using expanded
+    # dot product equation
+    dot_product_vector = [x*y for x,y in zip(v1.coordinates, v2.coordinates)]
+    return sum(dot_product_vector)
+
+def getAngle(v1, v2, d_or_r="r"):
+    try:
+        dot_product = getDotProduct(v1, v2)
+        product_of_magnitudes = v1.magnitude * v2.magnitude
+        if d_or_r=="d":
+            return math.degrees(math.acos(dot_product/product_of_magnitudes))
+        else:
+            return math.acos(dot_product/product_of_magnitudes)
+    except ZeroDivisionError:
+        raise Exception("One of the vector magnitudes is 0, so no angle exists")
+
+
 
 
 '''Test main function'''
 if __name__ == '__main__':
-    coordinates1 = (5.581, -2.136)
+    coordinates1 = (7.35, 0.221, 5.188)
     v1 = Vector(coordinates1)
-    print(v1.direction)
+
+    coordinates2 = (2.751, 8.259, 3.985)
+    v2 = Vector(coordinates2)
+
+    print(getAngle(v1, v2, "d"))
     #print(v1.direction)
 
     '''coordinates2 = (1.671, -1.012, -0.318)
